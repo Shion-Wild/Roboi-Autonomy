8b580d7576cfb5e82e0788bdb8169340bb0f2451
Matthew Anastasio <MatthewAnastasio@knights.ucf.edu>
2023-04-05 05:40:29 -0400

    Menu and enemy

---
+++ Team2Demo/Assets/Scripts/Pause.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+
+public class Pause : MonoBehaviour
+{
+
+    public GameObject pauseMenu;
+
+    public static bool isPaused;
+
+    void Start()
+    {
+        pauseMenu.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(isPaused)
+            {
+                resumeGame();
+            }
+            else
+            {
+                pauseGame();
+            }
+        }
+        
+    }
+
+    public void pauseGame()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+       // Cursor.visible = true;
+
+    }
+
+    public void resumeGame()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    // public void backToMenu()
+    // {
+    //     SceneManager.LoadScene(0);
+    // }
+}
