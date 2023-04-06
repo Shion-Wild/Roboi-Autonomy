bf762f9a542299430d5660a9baee0cbecffa4c85
krillin-avi <cesdaille@knights.ucf.edu>
2023-04-06 00:18:40 -0400

    Virus Established

--- Team2Demo/Assets/Scripts/ThirdPersonCam.cs
+++ Team2Demo/Assets/Scripts/ThirdPersonCam.cs
@@ -14,8 +14,8 @@ public class ThirdPersonCam : MonoBehaviour
 
     private void Start()
     {
-        //Cursor.lockState = CursorLockMode.Locked;
-        //Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     private void Update()
