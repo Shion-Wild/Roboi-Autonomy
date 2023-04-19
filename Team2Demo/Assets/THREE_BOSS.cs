e11dab7b559c3903868a0cad200b167637aa0a0e
krillin-avi <cesdaille@knights.ucf.edu>
2023-04-19 12:22:47 -0400

    Fix

---
+++ Team2Demo/Assets/THREE_BOSS.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class THREE_BOSS : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Character")
+        {
+
+            SceneManager.LoadScene(5);
+        }
+    }
+
+}
