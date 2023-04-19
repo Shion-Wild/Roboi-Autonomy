6007115e9688180cede8fcdc4409188f440ad837
Matthew Anastasio <MatthewAnastasio@knights.ucf.edu>
2023-04-19 14:37:08 -0400

    Hold it

--- Team2Demo/Assets/Scripts/Final Scripts/PlayerController.cs
+++ Team2Demo/Assets/Scripts/Final Scripts/PlayerController.cs
@@ -27,7 +27,7 @@ public class PlayerController : MonoBehaviour
     {
         if (currentHealth <= 0)
         {
-            SceneManager.LoadScene(6);
+            SceneManager.LoadScene(7);
         }
         
     }
