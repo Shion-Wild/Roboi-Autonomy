18275ca28e3fa44be037afb64ce494e599cc249b
krillin-avi <cesdaille@knights.ucf.edu>
2023-04-24 18:49:10 -0400

    Weather Commit

---
+++ Team2Demo/Assets/Scripts/Final Scripts/BossController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossController : MonoBehaviour
+{
+    public GameObject target1; // First target game object
+    public GameObject target2; // Second target game object
+    public GameObject target3; // Third target game object
+
+    private int targetsDestroyed; // Number of targets destroyed
+
+    void Start()
+    {
+        targetsDestroyed = 0; // Initialize targets destroyed to 0
+    }
+
+    void Update()
+    {
+        // Check if all three targets are destroyed
+        if (target1 == null && target2 == null && target3 == null)
+        {
+            Debug.Log("Boss defeated!"); // Display message when boss is defeated
+        }
+    }
+
+    // Called when a target is destroyed
+    public void TargetDestroyed()
+    {
+        targetsDestroyed++; // Increment targets destroyed counter
+
+        // Check if all three targets are destroyed
+        if (targetsDestroyed == 3)
+        {
+            Debug.Log("All targets destroyed!"); // Display message when all targets are destroyed
+            // Perform additional actions to defeat the boss here
+        }
+    }
+}
---
+++ Team2Demo/Assets/Scripts/Final Scripts/BossDestroyableObject.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossDestroyableObject : MonoBehaviour
+{
+
+    // Material Reference for Unique EMP'able Console
+    public Material targetMaterial;
+    public Color baseEmissionColor;
+    public Color newEmissionColor;
+
+
+    void Start()
+    {
+        SetEmissionColorAtStart();
+    }
+
+    void SetEmissionColorAtStart()
+    {
+        if (targetMaterial == null)
+        {
+            return;
+        }
+
+        // Set the base emission color at start
+        targetMaterial.SetColor("_EmissionColor", baseEmissionColor);
+        // Enable emission on the material
+        targetMaterial.EnableKeyword("_EMISSION");
+        // Update the material to apply the changes
+        targetMaterial.globalIlluminationFlags = MaterialGlobalIlluminationFlags.RealtimeEmissive;
+    }
+    public void EMPDestroyDoor()
+    {
+        if (targetMaterial == null)
+        {
+            Debug.LogError("Target Material is not assigned in the ChangeEmissionColor script!");
+            return;
+        }
+
+        // Set the new emission color
+        targetMaterial.SetColor("_EmissionColor", newEmissionColor);
+        // Enable emission on the material
+        targetMaterial.EnableKeyword("_EMISSION");
+        // Update the material to apply the changes
+        targetMaterial.globalIlluminationFlags = MaterialGlobalIlluminationFlags.RealtimeEmissive;
+
+    }
+
+    public void EMPDestroyBoss()
+    {
+        if (targetMaterial == null)
+        {
+            Debug.LogError("Target Material is not assigned in the ChangeEmissionColor script!");
+            return;
+        }
+
+        // Set the new emission color
+        targetMaterial.SetColor("_EmissionColor", newEmissionColor);
+        // Enable emission on the material
+        targetMaterial.EnableKeyword("_EMISSION");
+        // Update the material to apply the changes
+        targetMaterial.globalIlluminationFlags = MaterialGlobalIlluminationFlags.RealtimeEmissive;
+
+    }
+
+
+}
