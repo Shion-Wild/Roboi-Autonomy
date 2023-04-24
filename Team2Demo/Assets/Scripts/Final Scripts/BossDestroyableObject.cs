1f2bc248d14ffc0a226d4ac2b19bc7a118e9ec82
krillin-avi <cesdaille@knights.ucf.edu>
2023-04-24 19:41:20 -0400

    EMP Triggers

--- Team2Demo/Assets/Scripts/Final Scripts/BossDestroyableObject.cs
+++ Team2Demo/Assets/Scripts/Final Scripts/BossDestroyableObject.cs
@@ -30,22 +30,6 @@ public class BossDestroyableObject : MonoBehaviour
         // Update the material to apply the changes
         targetMaterial.globalIlluminationFlags = MaterialGlobalIlluminationFlags.RealtimeEmissive;
     }
-    public void EMPDestroyDoor()
-    {
-        if (targetMaterial == null)
-        {
-            Debug.LogError("Target Material is not assigned in the ChangeEmissionColor script!");
-            return;
-        }
-
-        // Set the new emission color
-        targetMaterial.SetColor("_EmissionColor", newEmissionColor);
-        // Enable emission on the material
-        targetMaterial.EnableKeyword("_EMISSION");
-        // Update the material to apply the changes
-        targetMaterial.globalIlluminationFlags = MaterialGlobalIlluminationFlags.RealtimeEmissive;
-
-    }
 
     public void EMPDestroyBoss()
     {
--- Team2Demo/Assets/Scripts/Final Scripts/EMPGrenade.cs
+++ Team2Demo/Assets/Scripts/Final Scripts/EMPGrenade.cs
@@ -8,7 +8,7 @@ public class EMPGrenade : MonoBehaviour
 
     public float delay = 2f;
     public float force = 800f;
-    public float radius = 20f;
+    public float radius = 5f;
 
     // Layer Mask for EMP Testing
     public int layerMask = 1 << 9;
@@ -39,23 +39,51 @@ public class EMPGrenade : MonoBehaviour
 
         Collider[] colliders = Physics.OverlapSphere(transform.position, radius, layerMask);
 
+
         foreach (Collider nearbyObject in colliders)
         {
-            DestroyableObject destroyableObject = nearbyObject.GetComponent<DestroyableObject>();
-            Rigidbody rb = nearbyObject.GetComponent<Rigidbody>();
-            EnemyAI enemyScript = nearbyObject.GetComponent<EnemyAI>();
-
-            if (rb != null)
+            // Check if the nearby object has a specific tag
+            if (nearbyObject.CompareTag("Enemy"))
             {
-                rb.AddExplosionForce(force, transform.position, radius);
+                // Get the components of the nearby object
+                Rigidbody rb = nearbyObject.GetComponent<Rigidbody>();
+                EnemyAI enemyScript = nearbyObject.GetComponent<EnemyAI>();
 
-                enemyScript.DisableEnemy();
-            }
-            if (destroyableObject != null)
-            {
-                destroyableObject.EMPDestroy();
-            }
+                // If the nearby object has a Rigidbody component
+                if (rb != null)
+                {
+                    // Apply explosion force to the Rigidbody
+                    rb.AddExplosionForce(force, transform.position, radius);
 
+                    // Call a method to disable the enemy's AI
+                    enemyScript.DisableEnemy();
+                }
+
+            } else if (nearbyObject.CompareTag("EMPDoor")) {
+
+                // Get the components of the nearby object
+                DestroyableObject destroyableObject = nearbyObject.GetComponent<DestroyableObject>();
+
+                // If the nearby object has a BossDestroyableObject component
+                if (destroyableObject != null)
+                {
+                    // Call a method to destroy the object as part of an EMP effect
+                    destroyableObject.EMPDestroyDoor();
+                }
+
+            } else if (nearbyObject.CompareTag("EMPBossHit")) {
+
+                // Get the components of the nearby object
+                BossDestroyableObject bossDestroyableObject = nearbyObject.GetComponent<BossDestroyableObject>();
+
+                // If the nearby object has a BossDestroyableObject component
+                if (bossDestroyableObject != null)
+                {
+                    // Call a method to destroy the object as part of an EMP effect
+                    bossDestroyableObject.EMPDestroyBoss();
+                }
+
+            }
         }
 
         Destroy(gameObject);
